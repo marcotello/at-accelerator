f93e55e0911afb18c12d6c5590054dfabc95294a
Marco Tello <marcotello5@outlook.com>
2024-04-11 23:30:15 -0600

    Adding the http call to fetch the most popular tv shows from the API

--- src/app/services/tv-shows-http.service.ts
+++ src/app/services/tv-shows-http.service.ts
@@ -1,53 +1,23 @@
 import { Injectable } from '@angular/core';
 import {TvShow} from "../models/tv-show.model";
-import {Observable, of} from "rxjs";
+import {map, Observable, of} from "rxjs";
+import {TvShowsApiResponse} from "../models/tv-shows-api-response.model";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TvShowsHttpService {
 
-  constructor() { }
+  private TV_SHOWS_API_URL = 'https://www.episodate.com/api/most-popular?page=1';
+
+  constructor(private http: HttpClient) { }
 
   public fetchMostPopularTVShows(): Observable<TvShow[]> {
-
-    //TODO: Change the mock data with the real API call
-    const tvShows: TvShow[] = [
-      {
-        id: 1,
-        name: 'Alone',
-        permalink: '',
-        start_date: '2015-01-01',
-        end_date: null,
-        country: 'US',
-        network: 'History',
-        status: 'Running',
-        image_thumbnail_path: 'https://example.com/image/alone.jpg'
-      },
-      {
-        id: 2,
-        name: 'Black Mirror',
-        permalink: '',
-        start_date: '2011-01-01',
-        end_date: null,
-        country: 'UK',
-        network: 'Netflix',
-        status: 'To Be Determined',
-        image_thumbnail_path: 'https://example.com/image/blackmirror.jpg'
-      },
-      {
-        id: 3,
-        name: 'True Detective',
-        permalink: '',
-        start_date: '2014-01-01',
-        end_date: null,
-        country: 'US',
-        network: 'HBO',
-        status: 'Running',
-        image_thumbnail_path: 'https://example.com/image/truedetective.jpg'
-      }
-    ];
-
-    return of(tvShows);
+    return this.http
+      .get<TvShowsApiResponse>(this.TV_SHOWS_API_URL)
+      .pipe(
+        map(response => response.tv_shows)
+      );
   }
 }
